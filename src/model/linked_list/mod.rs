68771c1f8536330b9785749b0d39792c2ab3fd3a
Anonyfox <hisako1337@gmail.com>
2020-04-15 00:10:26 +0200

    linkedlist seems to work

--- src/model/linked_list/mod.rs
+++ src/model/linked_list/mod.rs
@@ -5,8 +5,9 @@ use super::Model;
 use crate::backend::{Backend, DataBlock, StaticBlock};
 use header::Header;
 use node::Node;
-use std::error::Error;
 use std::marker::PhantomData;
+
+type Error = Box<dyn std::error::Error>;
 
 pub struct LinkedList<T> {
     header: Header,
@@ -21,12 +22,12 @@ where
     T: serde::Serialize,
     for<'de> T: serde::Deserialize<'de>,
 {
-    pub fn new(path: &str) -> Result<Self, Box<dyn Error>> {
+    pub fn new(path: &str) -> Result<Self, Error> {
         let backend = Self::connect_backend(&path)?;
         Self::initialize_state(backend)
     }
 
-    fn initialize_state(mut backend: Box<dyn Backend>) -> Result<Self, Box<dyn Error>> {
+    fn initialize_state(mut backend: Box<dyn Backend>) -> Result<Self, Error> {
         let mut header = Header::load(&*backend, 0)?;
         if header.element_count() == 0 {
             header.set_allocator(Header::size());
@@ -36,7 +37,6 @@ where
             header,
             backend,
             data_type: PhantomData,
-            // data_type: PhantomData,
         })
     }
 
@@ -44,69 +44,47 @@ where
         self.header.element_count()
     }
 
-    pub fn first_node(&self) -> Result<Option<Node<T>>, Box<dyn Error>> {
+    pub fn first_node(&self) -> Result<Option<Node<T>>, Error> {
         let position = self.header.get_first_node_ptr();
-        if position == 0 {
-            Ok(None)
-        } else {
-            let node = Node::load(&*self.backend, position)?;
-            Ok(Some(node))
-        }
+        let node = Node::load(&*self.backend, position)?;
+        Ok(Some(node))
     }
 
-    pub fn last_node(&self) -> Result<Option<Node<T>>, Box<dyn Error>> {
+    pub fn last_node(&self) -> Result<Option<Node<T>>, Error> {
         let position = self.header.get_last_node_ptr();
-        if position == 0 {
-            Ok(None)
-        } else {
-            let node = Node::load(&*self.backend, position)?;
-            Ok(Some(node))
-        }
+        let node = Node::load(&*self.backend, position)?;
+        Ok(Some(node))
     }
 
-    // pub fn insert_before(
-    //     &mut self,
-    //     node: &mut Node<T>,
-    //     data: &T,
-    // ) -> Result<Node<T>, Box<dyn Error>> {
-    //     let position = self.header.get_allocator();
-    //     let mut new_node = Node::<T>::create(&mut *self.backend, position)?;
-    //     if node.is_first() {
-    //         self.header.set_first_node_ptr(new_node.start());
-    //         self.header.save(&mut *self.backend)?;
-    //     }
-    //     node.set_prev(&mut *self.backend, &new_node)?;
-    //     new_node.set_next(&mut *self.backend, &node)?;
-    //     new_node.data_store(&mut *self.backend, &data);
-    //     Ok(new_node)
-    // }
-
-    pub fn push(&mut self, data: &T) -> Result<Node<T>, Box<dyn Error>> {
+    pub fn insert_before(&mut self, node: &mut Node<T>, data: &T) -> Result<Node<T>, Error> {
         let position = self.header.get_allocator();
-        let mut new_node = Node::create(&mut *self.backend, position)?;
-        if let Some(mut last_node) = self.last_node()? {
-            last_node.set_next(&mut *self.backend, &new_node)?;
-            new_node.set_prev(&mut *self.backend, &last_node)?;
-        };
+        let mut new_node = Node::<T>::create(&mut *self.backend, position)?;
+        if let Some(mut prev_node) = node.prev(&*self.backend)? {
+            prev_node.set_next(&mut *self.backend, &new_node)?;
+            new_node.set_prev(&mut *self.backend, &prev_node)?;
+        }
+        node.set_prev(&mut *self.backend, &new_node)?;
+        new_node.set_next(&mut *self.backend, &node)?;
         new_node.data_store(&mut *self.backend, &data)?;
         self.finalize_insert(&new_node)?;
         Ok(new_node)
     }
 
-    pub fn pop(&mut self) -> Result<Option<Node<T>>, Box<dyn Error>> {
-        if let Some(last_node) = self.last_node()? {
-            if let Some(mut prev_node) = last_node.prev(&mut *self.backend)? {
-                prev_node.set_next_empty(&mut *self.backend)?;
-            }
-            self.finalize_removal(&last_node)?;
-            Ok(Some(last_node))
-        } else {
-            Ok(None)
+    pub fn insert_after(&mut self, node: &mut Node<T>, data: &T) -> Result<Node<T>, Error> {
+        let position = self.header.get_allocator();
+        let mut new_node = Node::<T>::create(&mut *self.backend, position)?;
+        if let Some(mut next_node) = node.next(&*self.backend)? {
+            next_node.set_prev(&mut *self.backend, &new_node)?;
+            new_node.set_next(&mut *self.backend, &next_node)?;
         }
+        node.set_next(&mut *self.backend, &new_node)?;
+        new_node.set_prev(&mut *self.backend, &node)?;
+        new_node.data_store(&mut *self.backend, &data)?;
+        self.finalize_insert(&new_node)?;
+        Ok(new_node)
     }
 
-    // adds one item in front
-    pub fn unshift(&mut self, data: &T) -> Result<Node<T>, Box<dyn Error>> {
+    pub fn insert_start(&mut self, data: &T) -> Result<Node<T>, Error> {
         let position = self.header.get_allocator();
         let mut new_node = Node::create(&mut *self.backend, position)?;
         if let Some(mut first_node) = self.first_node()? {
@@ -118,20 +96,36 @@ where
         Ok(new_node)
     }
 
-    // removes one item from front
-    pub fn shift(&mut self) -> Result<Option<Node<T>>, Box<dyn Error>> {
-        if let Some(first_node) = self.first_node()? {
-            if let Some(mut next_node) = first_node.next(&*self.backend)? {
-                next_node.set_prev_empty(&mut *self.backend)?;
-            }
-            self.finalize_removal(&first_node)?;
-            Ok(Some(first_node))
-        } else {
-            Ok(None)
-        }
+    pub fn insert_end(&mut self, data: &T) -> Result<Node<T>, Error> {
+        let position = self.header.get_allocator();
+        let mut new_node = Node::create(&mut *self.backend, position)?;
+        if let Some(mut last_node) = self.last_node()? {
+            last_node.set_next(&mut *self.backend, &new_node)?;
+            new_node.set_prev(&mut *self.backend, &last_node)?;
+        };
+        new_node.data_store(&mut *self.backend, &data)?;
+        self.finalize_insert(&new_node)?;
+        Ok(new_node)
     }
 
-    fn finalize_insert(&mut self, new_node: &Node<T>) -> Result<(), Box<dyn Error>> {
+    pub fn remove(&mut self, node: Node<T>) -> Result<(), Error> {
+        if let Some(mut prev_node) = node.prev(&mut *self.backend)? {
+            if let Some(mut next_node) = node.next(&mut *self.backend)? {
+                next_node.set_prev(&mut *self.backend, &prev_node)?;
+                prev_node.set_next(&mut *self.backend, &next_node)?;
+            } else {
+                prev_node.set_next_empty(&mut *self.backend)?;
+            }
+        } else {
+            if let Some(mut next_node) = node.next(&mut *self.backend)? {
+                next_node.set_prev_empty(&mut *self.backend)?;
+            }
+        }
+        self.finalize_removal(&node)?;
+        Ok(())
+    }
+
+    fn finalize_insert(&mut self, new_node: &Node<T>) -> Result<(), Error> {
         if new_node.is_first() {
             self.header.set_first_node_ptr(new_node.start());
         }
@@ -145,7 +139,7 @@ where
         Ok(())
     }
 
-    fn finalize_removal(&mut self, old_node: &Node<T>) -> Result<(), Box<dyn Error>> {
+    fn finalize_removal(&mut self, old_node: &Node<T>) -> Result<(), Error> {
         if old_node.is_first() {
             let next_ptr = old_node
                 .next(&*self.backend)?
@@ -166,7 +160,7 @@ where
         Ok(())
     }
 
-    fn update_allocator(&mut self, node: &Node<T>) -> Result<(), Box<dyn Error>> {
+    fn update_allocator(&mut self, node: &Node<T>) -> Result<(), Error> {
         let position = node.start() + Node::<T>::size() + node.data_size();
         if position > self.header.get_allocator() {
             self.header.set_allocator(position);
@@ -181,82 +175,74 @@ mod tests {
     use super::*;
 
     #[test]
-    fn push_and_pop() {
+    fn insert_methods_left() {
         let mut list = LinkedList::<String>::new("works.list").expect("can not create");
         assert_eq!(list.count(), 0);
 
-        list.push(&"hello".to_string()).expect("couldn't push");
+        let mut node1 = list
+            .insert_start(&"hello".to_string())
+            .expect("couldn't insert start");
         assert_eq!(list.count(), 1);
+        assert!(node1.is_first());
+        assert!(node1.is_last());
 
-        list.push(&"world".to_string()).expect("couldn't push");
+        let node2 = list
+            .insert_before(&mut node1, &"world".to_string())
+            .expect("couldn't insert before");
         assert_eq!(list.count(), 2);
-
-        let node = list.pop().expect("couldn't pop");
-        let content = node.unwrap().data_fetch(&*list.backend).unwrap();
-        assert_eq!(content, "world");
-        assert_eq!(list.count(), 1);
-
-        let node = list.pop().expect("couldn't pop");
-        let content = node.unwrap().data_fetch(&*list.backend).unwrap();
-        assert_eq!(content, "hello");
-        assert_eq!(list.count(), 0);
+        assert!(!node1.is_first());
+        assert!(node2.is_first());
+        assert!(node1.is_last());
+        assert!(!node2.is_last());
     }
 
     #[test]
-    fn shift_and_unshift() {
-        let mut list = LinkedList::<usize>::new("works.list").expect("can not create");
+    fn insert_methods_right() {
+        let mut list = LinkedList::<String>::new("works.list").expect("can not create");
         assert_eq!(list.count(), 0);
 
-        list.unshift(&1).expect("couldn't unshift");
+        let mut node1 = list
+            .insert_end(&"hello".to_string())
+            .expect("couldn't insert start");
         assert_eq!(list.count(), 1);
+        assert!(node1.is_first());
+        assert!(node1.is_last());
 
-        list.unshift(&2).expect("couldn't unshift");
+        let node2 = list
+            .insert_after(&mut node1, &"world".to_string())
+            .expect("couldn't insert before");
         assert_eq!(list.count(), 2);
-
-        let node = list.shift().expect("couldn't shift").unwrap();
-        let content = node.data_fetch(&*list.backend).unwrap();
-        assert_eq!(content, 2);
-        assert_eq!(list.count(), 1);
-
-        let node = list.shift().expect("couldn't shift").unwrap();
-        let content = node.data_fetch(&*list.backend).unwrap();
-        assert_eq!(content, 1);
-        assert_eq!(list.count(), 0);
-    }
-    #[derive(serde::Serialize, serde::Deserialize, Debug)]
-    struct Example {
-        name: String,
-        score: usize,
+        assert!(node1.is_first());
+        assert!(!node2.is_first());
+        assert!(!node1.is_last());
+        assert!(node2.is_last());
     }
 
     #[test]
-
-    fn generic_data() {
-        let mut list = LinkedList::<Example>::new("works.list").expect("can not create");
+    fn remove() {
+        let mut list = LinkedList::<String>::new("works.list").expect("can not create");
         assert_eq!(list.count(), 0);
 
-        let example = Example {
-            name: "john".to_owned(),
-            score: 42,
-        };
-        list.push(&example).expect("couldn't push");
-        assert_eq!(list.count(), 1);
-
-        let example = Example {
-            name: "hans".to_owned(),
-            score: 43,
-        };
-        list.unshift(&example).expect("couldn't unshift");
+        let mut node1 = list
+            .insert_start(&"hello".to_string())
+            .expect("couldn't insert start");
+        let mut node2 = list
+            .insert_after(&mut node1, &"world".to_string())
+            .expect("couldn't insert before");
         assert_eq!(list.count(), 2);
+        assert!(node1.is_first());
+        assert!(node2.is_last());
 
-        let node = list.pop().expect("couldn't pop").unwrap();
-        let content = node.data_fetch(&*list.backend).unwrap();
-        assert_eq!(content.name, "john");
+        list.remove(node1).expect("couldn't remove");
+        node2
+            .init(&mut *list.backend)
+            .expect("could not refresh node");
         assert_eq!(list.count(), 1);
 
-        let node = list.shift().expect("couldn't pop").unwrap();
-        let content = node.data_fetch(&*list.backend).unwrap();
-        assert_eq!(content.name, "hans");
+        assert!(node2.is_first());
+        assert!(node2.is_last());
+
+        list.remove(node2).expect("couldn't remove");
         assert_eq!(list.count(), 0);
     }
 }
